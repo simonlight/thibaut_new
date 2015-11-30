83acc17cef4989506c9b4f2925db368c0e6ab170
xin_urbandancesquad <xin.wang@lip6.fr>
2015-12-01 00:44:01 +0100

    ...

--- src/lsvm/batch_run_stefan.sh
+++ src/lsvm/batch_run_stefan.sh
@@ -7,7 +7,7 @@ scale_arr=("60" "50" "40" "30")
 #k='oarsub -p "host='"'"'big17'"'"' or host='"'"'big18'"'"' or host='"'"'big19'"'"' or host='"'"'big20'"'"' " -l "nodes=1/core=1,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. 
 #data/uiuc/mac/StefanLSSVMMulticlassTestET'
 
-k='oarsub -p "host='"'"'big18'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/src:. 
+k='oarsub -p "host='"'"'big18'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvm/LSVM_console_ferrari/stefan'
 
 #k='oarsub -p "host='"'"'big$1'"'"' " -l "nodes=1/core=1,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. 
