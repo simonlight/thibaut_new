c8e2d274649f4075acc8ba22f7e79e309aefef9a
xin_urbandancesquad <xin.wang@lip6.fr>
2015-12-01 07:48:08 +0100

    big 18 ferrari lsvm

--- bin/lsvm/batch_run_ferrari.sh
+++ bin/lsvm/batch_run_ferrari.sh
@@ -2,7 +2,7 @@
 #cls_arr=("cat" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "diningtable" "bicycle")
 #cls_arr=("cow" "sofa" "diningtable" "bicycle")
 scale_arr=("50")
-cls_arr=("bicycle")
+cls_arr=("sofa")
 
 #scale_arr=("90")
 #k='oarsub -p "host='"'"'big17'"'"' or host='"'"'big18'"'"' or host='"'"'big19'"'"' or host='"'"'big20'"'"' " -l "nodes=1/core=1,walltime=500:0:0" --notify "mail:xin.wang@lip6.fr" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/lssvm_wang/src:. 
