456cda015d7b87847e05e22f35bbf8faab40eb08
xin_urbandancesquad <xin.wang@lip6.fr>
2015-12-10 12:10:46 +0100

    cv ferrari lsvm cccpgaze positive

--- bin/lsvmCCCPGazeVoc/batch_run_ferrari.sh
+++ bin/lsvmCCCPGazeVoc/batch_run_ferrari.sh
@@ -10,7 +10,7 @@ k='oarsub -p "host='"'"'big20'"'"' " -l "nodes=1/core=3,walltime=500:0:0" --noti
 lsvmCCCPGazeVoc/LSVM_console_ferrari'
 
 #k='oarsub -p "host='"'"'big20'"'"' " -l "nodes=1/core=8,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
-"lsvmCCCPGazeVoc/EvaluationLSVMFerrari'
+#"lsvmCCCPGazeVoc/EvaluationLSVMFerrari'
 
 
 end='"'
