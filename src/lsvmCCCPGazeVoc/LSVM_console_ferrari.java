40730ab08b6568b9a45622ce88c2177f72882572
xin_urbandancesquad <xin.wang@lip6.fr>
2015-12-18 10:35:01 +0100

    positive lsvm cccp bicycle ferrari

--- src/lsvmCCCPGazeVoc/LSVM_console_ferrari.java
+++ src/lsvmCCCPGazeVoc/LSVM_console_ferrari.java
@@ -34,7 +34,7 @@ public class LSVM_console_ferrari {
 	
 	String dataSource= "big";//local or other things
 	String gazeType = "ferrari";
-	String taskName = "lsvm_cccpgaze_negative_cv/";
+	String taskName = "lsvm_cccpgaze_positive_cv/";
 	double[] lambdaCV = {1e-4};
     double[] epsilonCV = {0};
     String[] classes = {args[0]};
--- src/lsvmCCCPGazeVoc/batch_run_ferrari.sh
+++ src/lsvmCCCPGazeVoc/batch_run_ferrari.sh
@@ -1,8 +1,8 @@
-cls_arr=("aeroplane" "cow" "dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
+#cls_arr=("aeroplane" "cow" "dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
 #cls_arr=("dog" "cat" "motorbike" "boat" "aeroplane" "horse" "cow" "sofa" "diningtable" "bicycle")
 #cls_arr=("dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
 scale_arr=("90" "80" "70" "60" "50" "40" "30")
-#cls_arr=("sofa")
+cls_arr=("bicycle")
 
 #scale_arr=("90")
 
@@ -12,7 +12,7 @@ scale_arr=("90" "80" "70" "60" "50" "40" "30")
 #k='oarsub -p "host='"'"'big20'"'"' " -l "nodes=1/core=8,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 #"lsvmCCCPGazeVoc/EvaluationLSVMFerrari'
 
-k='oarsub -p "host='"'"'big18'"'"' or host='"'"'big20'"'"' or host='"'"'big19'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+k='oarsub -p "host='"'"'big30'"'"' or host='"'"'big20'"'"' or host='"'"'big19'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc/LSVM_console_ferrari'
 
 
