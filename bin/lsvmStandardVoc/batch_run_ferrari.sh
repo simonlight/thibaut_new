2c03ce654af712afe700f0910a43134b52a9479c
xin_urbandancesquad <xin.wang@lip6.fr>
2016-01-13 00:29:54 +0100

    .

--- bin/lsvmStandardVoc/batch_run_ferrari.sh
+++ bin/lsvmStandardVoc/batch_run_ferrari.sh
@@ -1,7 +1,8 @@
 cls_arr=("aeroplane" "cow" "dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
 scale_arr=("30")
 
-#k='oarsub -p "host='"'"'big20'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+#k='oarsub -p "host='"'"'big20'"'"' " -l "n
+odes=1/core=4,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 #lsvmStandardVoc/EvaluationLSVMFerrari5Fold'
 
 k='oarsub -p "host='"'"'big20'"'"' or host='"'"'big18'"'"'" -l "nodes=1/core=6,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
