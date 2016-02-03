f3d8222fae1c76e2303617d2a8de64002061f59a
xin_urbandancesquad <xin.wang@lip6.fr>
2016-02-03 11:28:36 +0100

    lsvm_cccpgaze_posneg_cv_single_split_loss_divised_by_nb_of_examples

--- src/lsvmCCCPGazeVoc_PosNeg/batch_run_stefan.sh
+++ src/lsvmCCCPGazeVoc_PosNeg/batch_run_stefan.sh
@@ -2,7 +2,7 @@ cls_arr=("jumping" "phoning" "playinginstrument" "reading" "ridingbike" "ridingh
 #scale_arr=("90" "80" "70" "60" "50" "40" "30")
 scale_arr=("50")
 
-k='oarsub -p "host='"'"'big18'"'"' or host='"'"'big20'"'"' " -l "nodes=1/core=6,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+k='oarsub -p "host='"'"'big18'"'"' or host='"'"'big20'"'"' " -l "nodes=1/core=4,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc_PosNeg/LSVM_console_stefan'
 
 end='"'
