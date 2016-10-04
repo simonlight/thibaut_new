e82a09db3ba59dc71b2fd349ad3f195cdc81059c
xin_urbandancesquad <xin.wang@lip6.fr>
2016-10-04 19:01:05 +0200

    .

--- src/lsvmCCCPGazeVoc_PosNeg/batch_run_stefan.sh
+++ src/lsvmCCCPGazeVoc_PosNeg/batch_run_stefan.sh
@@ -3,7 +3,7 @@ scale_arr=("90" "80" "70" "60" "40" "30")
 #cls_arr=("jumping")
 #scale_arr=("90")
 
-k='oarsub -p "host='"'"'big20'"'"'  or host='"'"'big30'"'"' or host='"'"'big31'"'"' or host='"'"'big19'"'"' or host='"'"'big16'"'"' or host='"'"'big17'"'"' or host='"'"'big18'"'"' " -l "nodes=1/core=8,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+k='oarsub -p "host='"'"'big20'"'"'  or host='"'"'big30'"'"' or host='"'"'big31'"'"' or host='"'"'big19'"'"' or host='"'"'big16'"'"' or host='"'"'big17'"'"' or host='"'"'big18'"'"' " -l "nodes=1/core=12,walltime=500:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc_PosNeg/LSVM_console_stefan'
 
 end='"'
