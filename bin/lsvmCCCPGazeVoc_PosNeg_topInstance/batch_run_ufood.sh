f78b66deb7f183421b3fb487b5d636fdd6bad291
xin_urbandancesquad <xin.wang@lip6.fr>
2016-11-25 21:31:31 +0100

    food exp scale 40 30, cpu 8

--- bin/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_ufood.sh
+++ bin/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_ufood.sh
@@ -1,8 +1,8 @@
 cls_arr=("apple-pie" "bread-pudding" "beef-carpaccio" "beet-salad" "chocolate-cake" "chocolate-mousse" "donuts" "beignets" "eggs-benedict" "croque-madame" "gnocchi" "shrimp-and-grits" "grilled-salmon" "pork-chop" "lasagna" "ravioli" "pancakes" "french-toast" "spaghetti-bolognese" "pad-thai")
 #cls_arr=("beignets")
 #scale_arr=("100" "90" "80" "70" "60" "50" "40" "30")
-scale_arr=("60" "50")
-k='oarsub -p "host='"'"'big20'"'"'  or host='"'"'big30'"'"' or host='"'"'big31'"'"' or host='"'"'big19'"'"' or host='"'"'big16'"'"' or host='"'"'big17'"'"' or host='"'"'big18'"'"' " -l "nodes=1/core=4,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+scale_arr=("40" "30")
+k='oarsub -p "host='"'"'big20'"'"'  or host='"'"'big30'"'"' or host='"'"'big31'"'"' or host='"'"'big19'"'"' or host='"'"'big16'"'"' or host='"'"'big17'"'"' or host='"'"'big18'"'"' " -l "nodes=1/core=8,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc_PosNeg_topInstance/LSVM_console_ufood'
 end='"'
 space=' '
