e7ef69a1643b3e54a8deecb9dc35c691ecf2d0a1
xin_urbandancesquad <xin.wang@lip6.fr>
2016-12-09 17:47:32 +0100

    .

--- bin/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_stefan.sh
+++ bin/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_stefan.sh
@@ -3,7 +3,7 @@
 cls_arr=("jumping")
 scale_arr=("90")
 
-k='oarsub -p "   host='"'"'big20'"'"'  or host='"'"'big20'"'"'    " -l "nodes=1/core=1,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms2g -Xmx8000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+k='oarsub -p "   host='"'"'big20'"'"'  or host='"'"'big20'"'"'    " -l "nodes=1/core=1,walltime=100:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms2g -Xmx8000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc_PosNeg_topInstance/LSVM_console_stefan'
 
 end='"'
