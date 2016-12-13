f91478d1a24b8752ac63d5c0c864d6693053ad9d
xin_urbandancesquad <xin.wang@lip6.fr>
2016-12-13 13:55:21 +0100

    big for gt independent et loss

--- bin/lsvmCCCPGazeVoc_PosNeg_topInstance_GtIndependent_Bound/batch_run_ufood.sh
+++ bin/lsvmCCCPGazeVoc_PosNeg_topInstance_GtIndependent_Bound/batch_run_ufood.sh
@@ -3,7 +3,7 @@ cls_arr=("apple-pie" "bread-pudding" "beef-carpaccio" "beet-salad" "chocolate-ca
 #scale_arr=("100" "90" "80" "70" "60" "50" "40" "30")
 scale_arr=("100" "90" "80" "70" "60" "50")
 #scale_arr=("90")
-k='oarsub -p "host='"'"'big6'"'"'  or host='"'"'big9'"'"' host='"'"'big11'"'"'  or host='"'"'big12'"'"' host='"'"'big13'"'"'  or host='"'"'big14'"'"' host='"'"'big15'"'"'   " -l "nodes=1/core=1,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms2g -Xmx6000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+k='oarsub -p " host='"'"'big6'"'"'  or host='"'"'big9'"'"' or host='"'"'big11'"'"'  or host='"'"'big12'"'"' or host='"'"'big13'"'"'  or host='"'"'big14'"'"' or host='"'"'big15'"'"'   " -l "nodes=1/core=1,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms2g -Xmx6000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc_PosNeg_topInstance_GtIndependent_Bound/LSVM_console_ufood'
 end='"'
 space=' '
