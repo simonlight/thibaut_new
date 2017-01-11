4c957ac699840cac23d889b7f395522a0750eff6
xin_urbandancesquad <xin.wang@lip6.fr>
2017-01-11 19:44:07 +0100

    test

--- src/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_ferrari.sh
+++ src/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_ferrari.sh
@@ -1,4 +1,4 @@
-#cls_arr=("aeroplane" "cow" "dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
+cls_arr=("aeroplane" "cow" "dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
 scale_arr=("100" "90" "80" "70" "60" "50" "40" "30")
 
 k='oarsub -p "host='"'"'big7'"'"'  or host='"'"'big9'"'"' or host='"'"'big18'"'"' or host='"'"'big19'"'"' or host='"'"'big31'"'"'  " -l "nodes=1/core=3,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms5000m -Xmx12000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
