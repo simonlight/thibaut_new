8a3418f95f3a88484a9d13de71c1590750893180
xin_urbandancesquad <xin.wang@lip6.fr>
2017-02-01 13:34:54 +0100

    .

--- bin/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_ferrari.sh
+++ bin/lsvmCCCPGazeVoc_PosNeg_topInstance/batch_run_ferrari.sh
@@ -1,9 +1,9 @@
 #cls_arr=("aeroplane" "cow" "dog" "cat" "motorbike" "boat"  "horse"  "sofa" "diningtable" "bicycle")
-cls_arr=("diningtable" "dog")
+cls_arr=("aeroplane" "bicycle")
 #scale_arr=("100" "90" "80" "70" "60" "50" "40" "30")
-scale_arr=("40" )
+scale_arr=("30" )
 
-k='oarsub -p "  host='"'"'big7'"'"'  or host='"'"'big7'"'"' or host='"'"'big7'"'"'  " -l "nodes=1/core=6,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms32000m -Xmx32000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
+k='oarsub -p "  host='"'"'big19'"'"'  or host='"'"'big19'"'"' or host='"'"'big19'"'"'  " -l "nodes=1/core=6,walltime=1000:0:0" --notify "mail:biglip666@gmail.com" "/home/wangxin/lib/jdk1.8.0_25/bin/java -Xms40000m -Xmx40000m -XX:-UseGCOverheadLimit -classpath /home/wangxin/mosek/7/tools/platform/linux64x86/bin/mosek.jar:/home/wangxin/lib/commons-cli-1.2.jar:/home/wangxin/lib/jkernelmachines.jar:/home/wangxin/code/thibaut_new/bin:. 
 lsvmCCCPGazeVoc_PosNeg_topInstance/LSVM_console_ferrari'
 
 
